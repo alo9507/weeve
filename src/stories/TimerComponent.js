8e9d41991d00e265a111e0b8a54e79ab892412d5
Calvin Hoenes <calvinhoenes@gmail.com>
2020-06-23 21:57:03 +0200

    Initial commit

---
+++ src/stories/TimerComponent.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const TimerComponent = () => (
+  <svg width="250px" height="250px" viewBox="0 0 303 105">
+    <defs>
+      <style>
+        {
+          ".a{fill:#a1d9ed;font-size:16px;}.a,.b{font-family:IBMPlexMono-Bold, IBM Plex Mono;font-weight:700;}.b{fill:#151515;font-size:70px;}"
+        }
+      </style>
+    </defs>
+    <text className="a">
+      <tspan x={0} y={16}>
+        {"Time until next stage"}
+      </tspan>
+    </text>
+    <text className="b" transform="translate(0 14)">
+      <tspan x={0} y={72}>
+        {"04:59"}
+      </tspan>
+    </text>
+  </svg>
+);
+
+export default TimerComponent;
