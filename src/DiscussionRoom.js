e882189575d683871df3a922fcb266f79b152eaf
Calvin Hoenes <calvinhoenes@gmail.com>
2020-06-24 20:38:54 +0200

    rough first jitsi commit

--- src/DiscussionRoom.js
+++ src/DiscussionRoom.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useState } from 'react'
+import Jitsi from 'react-jitsi'
+
+const domain =  "167.172.10.231"
+const roomName = 'abi'
+const userFullName = 'Johann Strawberry'
 
 const DiscussionRoom = (props) => {
   var roomID = props.match.params.roomID;
@@ -6,6 +11,7 @@ const DiscussionRoom = (props) => {
   return (
     <div className="App">
       <h1>{`Discussion Room ${roomID}`}</h1>
+      <Jitsi roomName={roomName} displayName={userFullName} domain={domain} />
     </div>
   );
 };
