e1c38aac446408ded2d7c35b4ac41a02666e7ee3
Andrew <alo42@georgetown.edu>
2020-07-01 13:01:29 -0400

    refactoring to functional components and hooks; fixes child component render

--- src/DiscussionSession.js
+++ src/DiscussionSession.js
@@ -1,52 +1,48 @@
-import React, { useState } from 'react'
-import Jitsi from 'react-jitsi'
+import React, { useState, useEffect } from "react";
+import Jitsi from "react-jitsi";
 
-const domain =  "167.172.10.231"
-const userFullName = 'Johann Strawberry'
+const domain = "167.172.10.231";
+const userFullName = "Johann Strawberry";
 
-class DiscussionSession extends React.Component {
+const DiscussionSession = (props) => {
+  const [discussionID, setDiscussionID] = useState(
+    props.match.params.discussionID
+  );
 
-  constructor(props) {
-    super(props);
+  const [currentRoom, setCurrentRoom] = useState(
+    props.match.params.roomID ? props.match.params.roomID : "room1"
+  );
 
-    // Load RoomIDs + Participants from controller server indexed by discussionID
-    let currentRoom = props.match.params.roomID ? props.match.params.roomID : "room1";
-    let nextRoom = "room2";
+  const [nextRoom, setNextRoom] = useState("room2");
+  const [jitsiAPI, setJitsiAPI] = useState("");
 
-    console.log(props.match.params);
-
-    this.state = {
-      discussionID: props.match.params.discussionID,
-      currentRoom: currentRoom,
-      nextRoom: nextRoom,
-      jitsiAPI: ''
-    };
-
-    this.handleAPI = this.handleAPI.bind(this);
-    this.switchToNext = this.switchToNext.bind(this);
-  }
-
-  handleAPI(JitsiMeetAPI) {
-    this.setState({jitsiAPI: JitsiMeetAPI});
+  const handleAPI = (JitsiMeetAPI) => {
+    setJitsiAPI(JitsiMeetAPI);
   };
 
-  switchToNext() {
+  const switchToNext = () => {
     // Important to Dispose the backend and transport
-    this.state.jitsiAPI.dispose();
-    // TODO Crap approach, we should keep state and only re-render the Jitsi Component
-    this.props.history.push("/"+this.state.discussionID+"/"+this.state.nextRoom);
-    window.location.reload(false);
-  }
+    jitsiAPI.dispose();
 
-  render() {
-    return (
-      <div className="App">
-        <h1>{`Discussion ${this.state.discussionID} in Room ${this.state.currentRoom}`}</h1>
-        <Jitsi onAPILoad={this.handleAPI} roomName={this.state.currentRoom} displayName={userFullName} domain={domain} />
-        <a href="#" onClick={() => this.switchToNext()}>Switch to Next Room</a>
-      </div>
-    );
-  }
-}
+    // TODO Crap approach, we should keep state and only re-render the Jitsi Component
+    props.history.push("/" + discussionID + "/" + nextRoom);
+    window.location.reload(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>{`Discussion ${discussionID} in Room ${currentRoom}`}</h1>
+      <Jitsi
+        onAPILoad={handleAPI}
+        roomName={currentRoom}
+        displayName={userFullName}
+        domain={domain}
+      />
+      <a href="#" onClick={() => switchToNext()}>
+        Switch to Next Room
+      </a>
+    </div>
+  );
+};
 
 export default DiscussionSession;
